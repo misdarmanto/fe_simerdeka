f97403caa5624bebc529fdcbd356ed1b3736f3f2
misdarmanto <misdarmanto1922@gmail.com>
2023-06-01 23:48:49 +0700

    add summary

--- src/routers/home/index.tsx
+++ src/routers/home/index.tsx
@@ -1,26 +1,50 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BiUser } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import { MbkmProgramIcon, StudenIcon, StudyProgramIcon } from "../../components";
+import { ServiceHttp } from "../../services/api";
+
+interface SummariesTypes {
+	total_student: number;
+	total_study_program: number;
+	total_program: number;
+}
 
 const Home = () => {
 	const navigation = useNavigate();
+	const httpService = new ServiceHttp();
+
+	const [summaries, setSummaries] = useState<SummariesTypes>();
+
+	const fecthSummaries = async () => {
+		const result = await httpService.get({
+			path: "/summaries",
+		});
+		console.log(result);
+		setSummaries(result);
+	};
+
 	const isAuth = true;
 	useEffect(() => {
 		if (!isAuth) {
 			navigation("/login");
 		}
+		fecthSummaries();
 	}, []);
+
 	return (
 		<div className="flex flex-wrap">
 			<div className="flex items-center bg-white border border-gray-200 rounded-lg shadow m-5 p-8 w-64">
-				<StudenIcon size={30} className="mr-5" /> <p>1000 Mahasiswa</p>
+				<StudenIcon size={30} className="mr-5" />{" "}
+				<p>{summaries?.total_student} Mahasiswa</p>
 			</div>
 			<div className="flex items-center bg-white border border-gray-200 rounded-lg shadow m-5 p-8 w-64">
-				<StudyProgramIcon size={30} className="mr-5" /> <p>10 prodi</p>
+				<StudyProgramIcon size={30} className="mr-5" />{" "}
+				<p>{summaries?.total_study_program} Prodi</p>
 			</div>
 			<div className="flex items-center bg-white border border-gray-200 rounded-lg shadow m-5 p-8 w-64">
-				<MbkmProgramIcon size={30} className="mr-5" /> <p>5 Program MBKM</p>
+				<MbkmProgramIcon size={30} className="mr-5" />{" "}
+				<p>{summaries?.total_program} Program MBKM</p>
 			</div>
 		</div>
 	);
